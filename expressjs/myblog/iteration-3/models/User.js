631e51edb5b79ca56ffbb6e36bfab4be190b2f37
imamfarisi19 <edwardindo1@gmail.com>
2023-02-18 07:38:53 +0700

    create login

---
+++ expressjs/myblog/iteration-3/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const UserSChema = new Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+UserSChema.pre('save', function(next) {
+    const user = this
+
+    bcrypt.hash(user.password, 100, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
+
+// export model
+const User = mongoose.model('User', UserSChema)
+module.exports = User
