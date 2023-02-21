f7b01f842634d1362edc43c4abe79b88127e7966
imamfarisi19 <edwardindo1@gmail.com>
2023-02-21 23:51:14 +0700

    connect to atlas mongodb

--- expressjs/myblog/iteration-4/index.js
+++ expressjs/myblog/iteration-4/index.js
@@ -13,7 +13,7 @@ const authMiddleware = require("./middleware/authMIddleware")
 const ifAuthenticatedMIddleware = require("./middleware/ifAuthenticatedMiddleware")
 
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/my_database', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://test123:test12345@cluster0.lso0uoq.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
