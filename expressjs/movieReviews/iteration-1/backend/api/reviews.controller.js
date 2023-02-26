f61f81ea64cad20ec79b8e1aa104872853613a97
imamfarisi19 <edwardindo1@gmail.com>
2023-02-27 04:42:27 +0700

    create add, update, delete controller for reviews

--- expressjs/movieReviews/iteration-1/backend/api/reviews.controller.js
+++ expressjs/movieReviews/iteration-1/backend/api/reviews.controller.js
@@ -1,18 +1,13 @@
 import ReviewsDAO from '../dao/reviewsDAO.js'
-
 export default class ReviewsController {
     static async apiPostReview(req, res, next) {
         try {
             const movieId = req.body.movie_id
             const review = req.body.review
             const userInfo = {
-
-
                 name: req.body.name,
                 _id: req.body.user_id
-
             }
-
             const date = new Date()
             const ReviewResponse = await ReviewsDAO.addReview(
                 movieId,
@@ -24,6 +19,7 @@ export default class ReviewsController {
         } catch (e) {
             res.status(500).json({ error: e.message })
         }
+
     }
 
     static async apiUpdateReview(req, res, next) {
@@ -42,10 +38,7 @@ export default class ReviewsController {
                 res.status.json({ error })
             }
             if (ReviewResponse.modifiedCount === 0) {
-
-
                 throw new Error("unable to update review. User may not be original poster")
-
             }
             res.json({ status: "success " })
         } catch (e) {
@@ -65,6 +58,7 @@ export default class ReviewsController {
         } catch (e) {
             res.status(500).json({ error: e.message })
         }
+
     }
 
 }
