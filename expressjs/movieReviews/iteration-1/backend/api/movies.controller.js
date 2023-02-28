93183216301de701169e1e6d2e19de14d7d1c595
imamfarisi19 <edwardindo1@gmail.com>
2023-02-28 07:48:21 +0700

    movies controller add functions apiGetMovieById and apiGetRatings

--- expressjs/movieReviews/iteration-1/backend/api/movies.controller.js
+++ expressjs/movieReviews/iteration-1/backend/api/movies.controller.js
@@ -5,10 +5,7 @@ export default class MoviesController {
         const page = req.query.page ? parseInt(req.query.page) : 0
         let filters = {}
         if (req.query.rated) {
-
-
             filters.rated = req.query.rated
-
         } else if (req.query.title) {
             filters.title = req.query.title
         }
@@ -25,5 +22,32 @@ export default class MoviesController {
             total_results: totalNumMovies,
         }
         res.json(response)
+    }
+
+    static async apiGetMovieById(req, res, next) {
+        try {
+            let id = req.params.id || {}
+            let movie = await MoviesDAO.getMovieById(id)
+            if (!movie) {
+                res.status(404).json({ error: "not found" })
+                return
+            }
+            res.json(movie)
+
+
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+
+    }
+    static async apiGetRatings(req, res, next) {
+        try {
+            let propertyTypes = await MoviesDAO.getRatings()
+            res.json(propertyTypes)
+        } catch (e) {
+            console.log(`api,${e}`)
+            res.status(500).json({ error: e })
+        }
     }
 }
