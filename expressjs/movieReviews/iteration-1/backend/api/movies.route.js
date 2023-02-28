652211bbf671562a5d84a4eda2cf997f6b081746
imamfarisi19 <edwardindo1@gmail.com>
2023-02-28 07:49:03 +0700

    movies route add routers id and ratings

--- expressjs/movieReviews/iteration-1/backend/api/movies.route.js
+++ expressjs/movieReviews/iteration-1/backend/api/movies.route.js
@@ -5,10 +5,13 @@ import ReviewsController from './reviews.controller.js'
 const router = express.Router()
 
 router.route('/').get(MoviesController.apiGetMovies)
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+router.route("/ratings").get(MoviesController.apiGetRatings)
 
 router
     .route("/review")
     .post(ReviewsController.apiPostReview)
     .put(ReviewsController.apiUpdateReview)
     .delete(ReviewsController.apiDeleteReview)
+
 export default router
