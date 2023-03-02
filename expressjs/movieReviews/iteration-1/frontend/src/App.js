fed1d17fc22ef6fc778caab398ec187bd502ad7f
imamfarisi19 <edwardindo1@gmail.com>
2023-03-03 06:56:42 +0700

    make new components for application; movies-list, movie, add-review, and login

--- expressjs/movieReviews/iteration-1/frontend/src/App.js
+++ expressjs/movieReviews/iteration-1/frontend/src/App.js
@@ -2,23 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
     </div>
   );
 }
---
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MoviesList() {
+    return (
+        <div className="App">
+            MovieList
+        </div>
+    );
+}
+
+export default MoviesList;
