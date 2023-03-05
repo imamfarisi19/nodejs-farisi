776ad15c03dfac1d40b3590eb721e28f620d0d29
imamfarisi19 <edwardindo1@gmail.com>
2023-03-05 14:44:15 +0700

    me line of codes prettier

--- expressjs/movieReviews/iteration-1/frontend/src/index.js
+++ expressjs/movieReviews/iteration-1/frontend/src/index.js
@@ -22,7 +22,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <BrowserRouter>
