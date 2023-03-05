f2ccbba24f7e51c82a86bf576db11fc7a4aea105
imamfarisi19 <edwardindo1@gmail.com>
2023-03-05 15:16:03 +0700

    form jsx markup

--- expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import MovieDataservice from "../services/movies";
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 const MovieList = (props) => {
   const [movies, setMovies] = useState([]);
@@ -35,20 +40,56 @@ const MovieList = (props) => {
         console.log(e);
       });
   };
+
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+
+  const onChangeSearchRating = (e) => {
+    const searchRating = e.target.value;
+    setSearchRating(searchRating);
+  };
+
+  return (
+    <div className="App">
+      <Container>
+        <Form>
+          <Row>
+            <col>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  placeholder="Search by title"
+                  value={searchTitle}
+                  onChange={onChangeSearchTitle}
+                />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={findByTitle}>
+                Search
+              </Button>
+            </col>
+            <col>
+              <form.Group>
+                <form.Control as="select" onChange={onChangeSearchRating}>
+                  {ratings.map((rating) => {
+                    return <option value={rating}>{rating}</option>;
+                  })}
+                </form.Control>
+              </form.Group>
+              <button variant="primary" type="button" onClick={findByRating}>
+                Search
+              </button>
+            </col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
 };
 
 function MoviesList() {
   return <div className="App">MovieList</div>;
-}
-
-const onChangeSearchTitle = e => {
-    const searchTitle = e.target.value
-    setSearchTitle(searchTitle);
-}
-
-const onChangeSearchRating = e => {
-    const searchRating = e.target.value
-    setSearchRating(searchRating);
 }
 
 export default MoviesList;
