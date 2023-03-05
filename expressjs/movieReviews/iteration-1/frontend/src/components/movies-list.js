b53fdf46b1319bdf48dfc4d4488304150599c219
imamfarisi19 <edwardindo1@gmail.com>
2023-03-05 14:53:57 +0700

    implementin MoivesList component

--- expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import MovieDataservice from "../services/movies";
+import { Link } from "react-router-dom";
+
+const MovieList = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState("");
+  const [ratings, setRatings] = useState(["All Ratings"]);
+};
 
 function MoviesList() {
-    return (
-        <div className="App">
-            MovieList
-        </div>
-    );
+  return <div className="App">MovieList</div>;
 }
 
 export default MoviesList;
