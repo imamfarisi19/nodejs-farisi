27d55fbcc397cf372dc388338da2e74695f25875
imamfarisi19 <edwardindo1@gmail.com>
2023-03-05 15:05:42 +0700

    creating the search form

--- expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movies-list.js
@@ -7,10 +7,48 @@ const MovieList = (props) => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
+
+  useEffect(() => {
+    retrieveMovies();
+    retrieveRatings();
+  }, []);
+
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then((response) => {
+        console.log(response.data);
+        // start with 'All ratings' if user doesn't specify any ratings
+        setRatings(["All Ratings"].concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 };
 
 function MoviesList() {
   return <div className="App">MovieList</div>;
+}
+
+const onChangeSearchTitle = e => {
+    const searchTitle = e.target.value
+    setSearchTitle(searchTitle);
+}
+
+const onChangeSearchRating = e => {
+    const searchRating = e.target.value
+    setSearchRating(searchRating);
 }
 
 export default MoviesList;
