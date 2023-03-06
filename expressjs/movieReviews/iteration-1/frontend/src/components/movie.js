9792f1a564fb371cef4243462bb17b4f2dfe56a2
imamfarisi19 <edwardindo1@gmail.com>
2023-03-06 14:36:09 +0700

    have a movie state variable to hold the specific movie we are currently showing in the movie component

--- expressjs/movieReviews/iteration-1/frontend/src/components/movie.js
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movie.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "reat-router-dom";
 
-function Movie(){
-    return (
-        <div className="App">
-            Movie
-        </div>
-    );
-}
+const Movie = (props) => {
+  const [movie, setMovie] = useState({
+    id: null,
+    title: "",
+    rated: "",
+    reviews: [],
+  });
+
+  const getMovie = (id) => {
+    MovieDataService.get(id)
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getMovie(props.match.params.id);
+  }, [props.match.paramss.id]);
+
+  return <div className="App">Movie</div>;
+};
 
 export default Movie;
