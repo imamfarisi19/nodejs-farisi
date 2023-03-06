0a3991424ba0f540a8ab1420743440da933a96fd
imamfarisi19 <edwardindo1@gmail.com>
2023-03-06 14:45:42 +0700

    make two column; the first column contains the movie poster (if it exists) and the second column show the movie details in a Card component.

--- expressjs/movieReviews/iteration-1/frontend/src/components/movie.js
+++ expressjs/movieReviews/iteration-1/frontend/src/components/movie.js
@@ -1,6 +1,13 @@
 import React from "react";
 import MovieDataService from "../services/movies";
 import { Link } from "reat-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/esm/Button";
+import Media from "react-bootstrap/Media";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({
@@ -25,7 +32,32 @@ const Movie = (props) => {
     getMovie(props.match.params.id);
   }, [props.match.paramss.id]);
 
-  return <div className="App">Movie</div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <col>
+            <image src={movie.poster + "/100px250"} fluid />
+          </col>
+          <col>
+            <card>
+              <Card.Header as="h5">{movie.title}</Card.Header>
+              <card.body>
+                <card.text>{movie.plot}</card.text>
+                {props.user && (
+                  <Link to={"/movies/" + props.match.params.id + "/review"}>
+                    Add Review
+                  </Link>
+                )}
+              </card.body>
+            </card>
+            <br></br>
+            <h2>Reviews</h2>
+          </col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Movie;
